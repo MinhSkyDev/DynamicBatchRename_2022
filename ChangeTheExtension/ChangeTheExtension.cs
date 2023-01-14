9e2578b6afa8a8261339620af1ea1fb73b44c0d0
MinhSkyDev <minhdev2002@gmail.com>
2023-01-14 11:40:26 +0700

    add rule ChangeExtension

---
+++ ChangeTheExtension/ChangeTheExtension.cs
@@ -0,0 +1,41 @@
+﻿using IRenameRules_namepsace;
+
+namespace ChangeTheExtension
+{
+    public class ChangeTheExtension : IRenameRules
+    {
+        public string Extension { get; set; }
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public string getName()
+        {
+            return "ChangeTheExtension";
+        }
+
+        public void parseData(string data)
+        {
+            this.Extension = data;
+        }
+
+        public string Rename(string filename)
+        {
+            string newFilename = filename;
+            int index = newFilename.LastIndexOf('.') + 1; //get index start extension
+            if (index != -1)
+            {
+                string preExtension = newFilename.Substring(index, newFilename.Length - index);//get extension
+                newFilename = newFilename.Remove(index, preExtension.Length);
+                newFilename = newFilename.Insert(index, Extension);
+            }
+            return newFilename;
+        }
+
+        public string stringPrototype()
+        {
+            return "()";
+        }
+    }
+}
--- ConvertLowercaseAndRemoveSpace/ConvertLowercaseAndRemoveSpace.cs
+++ ConvertLowercaseAndRemoveSpace/ConvertLowercaseAndRemoveSpace.cs
@@ -42,7 +42,7 @@ namespace ConvertLowercaseAndRemoveSpace
 
         public string stringPrototype()
         {
-            return "";
+            return "()";
         }
     }
 }
